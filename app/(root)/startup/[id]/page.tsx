aeb7dcb890512b81947a7291f612b10f3012e8ec
pavan <pavanvenkatanagamanoj@gmail.com>
2025-06-17 22:46:53 +0530

    developed details page hero section and image section

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -13,36 +13,43 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   return (
     <>
       <section className="pink_container !min-h-[230px]">
-        <p className="tag"> {formatDate(post?._createdAt)}</p>
-        <h1 className="heading">{post?.title}</h1>
-        <p className="sub-heading !max-w-5xl">{post?.descreption}</p>
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+
+        <h1 className="heading">{post.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post.descreption}</p>
       </section>
+
       <section className="section_container">
         <img
           src={post.image}
           alt="thumbnail"
           className="w-full h-auto rounded-xl"
         />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
-          <Link
-            href={`/user/${post.author?._id}`}
-            className="flex gap-2 items-center mb-3"
-          >
-            <Image
-              src={post.author.image}
-              alt="avatar"
-              width={64}
-              height={64}
-              className="rounded-full drop-shadow-lg"
-            />
-            <div>
-              <p className="text-20-medium">{post.author.name}</p>
-              <p className="text-16-medium !text-black-300">
-                @{post.author?.username}
-              </p>
-            </div>
-          </Link>
-          <p className="category-tag">{post.category}</p>
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${post.author?._id}`}
+              className="flex gap-2 items-center mb-3"
+            >
+              <Image
+                src={post.author.image}
+                alt="avatar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+
+              <div>
+                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post.author.username}
+                </p>
+              </div>
+            </Link>
+
+            <p className="category-tag">{post.category}</p>
+          </div>
         </div>
       </section>
     </>
