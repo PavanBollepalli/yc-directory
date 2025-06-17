901e0d1315f15a6ef8df656f51939389e9c5f5a1
pavan <pavanvenkatanagamanoj@gmail.com>
2025-06-17 21:11:45 +0530

    working on startup details page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/utils";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -9,9 +12,39 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   if (!post) return notFound();
   return (
     <>
-      <h1 className="text-3xl">title: {post.title}</h1>
-      <h1 className="text-3xl">views: {post.views}</h1>
-      <h1 className="text-3xl">pitch: {post.pitch}</h1>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag"> {formatDate(post?._createdAt)}</p>
+        <h1 className="heading">{post?.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post?.descreption}</p>
+      </section>
+      <section className="section_container">
+        <img
+          src={post.image}
+          alt="thumbnail"
+          className="w-full h-auto rounded-xl"
+        />
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <Link
+            href={`/user/${post.author?._id}`}
+            className="flex gap-2 items-center mb-3"
+          >
+            <Image
+              src={post.author.image}
+              alt="avatar"
+              width={64}
+              height={64}
+              className="rounded-full drop-shadow-lg"
+            />
+            <div>
+              <p className="text-20-medium">{post.author.name}</p>
+              <p className="text-16-medium !text-black-300">
+                @{post.author?.username}
+              </p>
+            </div>
+          </Link>
+          <p className="category-tag">{post.category}</p>
+        </div>
+      </section>
     </>
   );
 };
